cc8b96d6d6aa8a0869a2a10881ce0d5726a24a87
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-06-03 23:20:14 +0300

    regex-nickname-2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ci test demo
+
+[![Build status](https://ci.appveyor.com/api/projects/status/rnv26xtt4fhuambn?svg=true)](https://ci.appveyor.com/project/Light969/regex-nickname)
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,29 @@
+/* eslint-disable no-useless-escape */
 export default class Validator {
   validateUsername(name) {
-    if (name.search(/^[a-z0-9\_\-]+$/i) === -1) {
+    // const exeptThreeNumbersInRow = /\w+([0-9])([0-9])([0-9])\w+/;
+    const exeptThreeNumbersInRow = /\w+(\d{3,})\w+/;
+    const dashAccept = /\w+-+\w+/;
+    const underlineAccept = /\w+_+\w+/;
+    const numbersAccept = /\w+[0-9]+\w+/;
+    const latinLettersAccept = /^[a-z]+$/i;
+    const exeptStartNumbersDashUnderline = /^[0-9\_\-]/;
+    const exeptEndNumbersDashUnderline = /[0-9\_\-]$/;
+
+    // if (name.search(/^[a-z0-9\_\-]+$/i) === -1) {
+    if (name.search(latinLettersAccept && numbersAccept && dashAccept && underlineAccept) === -1) {
       throw new Error('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
     }
 
-    if (name.search(/\w+([0-9])([0-9])([0-9])\w+/) === 0) {
+    if (name.search(exeptThreeNumbersInRow) === 0) {
       throw new Error('Имя не должно содержать подряд более трёх цифр');
     }
 
-    if (name.match(/^[0-9\_\-]/) !== null) {
+    if (name.match(exeptStartNumbersDashUnderline) !== null) {
       throw new Error('Имя не должно начинаться цифрами, символами подчёркивания или тире');
     }
 
-    if (name.match(/[0-9\_\-]$/) !== null) {
+    if (name.match(exeptEndNumbersDashUnderline) !== null) {
       throw new Error('Имя не должно заканчиваться цифрами, символами подчёркивания или тире');
     }
 
